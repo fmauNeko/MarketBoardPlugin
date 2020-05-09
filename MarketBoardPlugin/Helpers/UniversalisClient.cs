e6dbba4d0c75732b5e012095dd5d84dc980542d7
Florian Maunier <f.maunier@spi.immo>
2020-05-09 17:46:34 +0200

    ✨ (universalis) add cancellation token

--- MarketBoardPlugin/Helpers/UniversalisClient.cs
+++ MarketBoardPlugin/Helpers/UniversalisClient.cs
@@ -6,6 +6,7 @@ namespace MarketBoardPlugin.Helpers
 {
   using System;
   using System.Net.Http;
+  using System.Threading;
   using System.Threading.Tasks;
 
   using MarketBoardPlugin.Models.Universalis;
@@ -18,17 +19,25 @@ namespace MarketBoardPlugin.Helpers
   public static class UniversalisClient
   {
     /// <summary>
-    /// Gets market info of an item for a specific world.
+    /// Gets market data of an item for a specific world.
     /// </summary>
     /// <param name="itemId">The item ID.</param>
     /// <param name="worldName">The world's name.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/>.</param>
     /// <returns>The market data.</returns>
-    public static async Task<MarketDataResponse> GetMarketInfo(int itemId, string worldName)
+    public static async Task<MarketDataResponse> GetMarketData(int itemId, string worldName, CancellationToken cancellationToken)
     {
       var uriBuilder = new UriBuilder($"https://universalis.app/api/{worldName}/{itemId}");
 
+      cancellationToken.ThrowIfCancellationRequested();
+
       using var client = new HttpClient();
-      var res = await client.GetStringAsync(uriBuilder.Uri).ConfigureAwait(false);
+      var res = await client
+        .GetStringAsync(uriBuilder.Uri)
+        .ConfigureAwait(false);
+
+      cancellationToken.ThrowIfCancellationRequested();
+
       var parsedRes = JsonConvert.DeserializeObject<MarketDataResponse>(res);
 
       return parsedRes;

95c0963d77528c1f95b2114630298806a2c9af94
Yarukon <a2692866561@live.com>
2025-06-28 16:34:03 +0800

    add support for Chinese Cross-DC, changed the way to determine DC Name

--- MarketBoardPlugin/GUI/MarketBoardWindow.cs
+++ MarketBoardPlugin/GUI/MarketBoardWindow.cs
@@ -686,7 +686,7 @@ namespace MarketBoardPlugin.GUI
 
                   if (this.selectedWorld == 0)
                   {
-                    retainerSB.Append(CultureInfo.CurrentCulture, $" @ {this.GetDcNameFromWorldName(listing.WorldName)}");
+                    retainerSB.Append(CultureInfo.CurrentCulture, $" @ {this.GetDcNameFromWorldId(listing.WorldID)}");
                   }
                 }
                 else
@@ -1101,6 +1101,7 @@ namespace MarketBoardPlugin.GUI
           2 => "North-America",
           3 => "Europe",
           4 => "Oceania",
+          5 => "中国",
           _ => string.Empty,
         };
 
@@ -1238,9 +1239,9 @@ namespace MarketBoardPlugin.GUI
         cancellationToken);
     }
 
-    private string GetDcNameFromWorldName(string worldName)
+    private string GetDcNameFromWorldId(int worldId)
     {
-      var world = this.plugin.DataManager.GetExcelSheet<World>().FirstOrNull(w => w.Name.ExtractText() == worldName);
+      var world = this.plugin.DataManager.GetExcelSheet<World>().FirstOrNull(w => w.RowId == worldId);
 
       if (world != null)
       {
